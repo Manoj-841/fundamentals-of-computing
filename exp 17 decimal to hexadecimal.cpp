a16f031fee50552c71f005f3fcd7275bb0786015
Manoj-841 <113409747+Manoj-841@users.noreply.github.com>
2023-02-10 11:26:50 +0530

    Add files via upload

---
+++ exp 17 decimal to hexadecimal.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int decimalNumber;
+	printf("enter the decimal number");
+	scanf("%d",&decimalNumber);
+	printf("HexaDecimal number is:%X",decimalNumber);
+	return 0;
+}
