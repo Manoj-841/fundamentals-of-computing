37677036121c284344606679232449bac5e1097c
Manoj-841 <113409747+Manoj-841@users.noreply.github.com>
2023-02-10 11:12:03 +0530

    Add files via upload

---
+++ exp2 even.cpp
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("enter the value of limit");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)#include<stdio.h>
+int main()
+{
+	int n,a[20],i,sum=0;
+	printf("enter n elements:");
+	scanf("%d",&n);
+	printf("elements are:");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<=n;i++)
+	{
+		a[i] = i * i * i;
+		sum = sum + a[i];
+	}
+	printf("sum of the number series:%d",sum);
+}
+
+	{
+	   if(i%2==0)
+	   {
+		printf("%d,",i);
+	}
+}
+}
